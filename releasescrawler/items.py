b51a3061ebe8b50065ea930dcbbf3ff9d9297a11
Adriano Teodoro <adrianopteodoro@gmail.com>
2018-07-22 03:30:18 -0300

    Added more fields

--- releasescrawler/items.py
+++ releasescrawler/items.py
@@ -3,8 +3,12 @@ import scrapy
 
 
 class ReleasescrawlerItem(scrapy.Item):
+    rid = scrapy.Field()
     name = scrapy.Field()
     description = scrapy.Field()
-    release_date = scrapy.Field()
-    platform = scrapy.Field()
     tags = scrapy.Field()
+    release_version = scrapy.Field()
+    release_country = scrapy.Field()
+    release_date = scrapy.Field()
+    release_status = scrapy.Field()
+    release_platform = scrapy.Field()
